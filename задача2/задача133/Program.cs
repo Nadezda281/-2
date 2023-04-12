8b776183071d24d7f0c841ed5bcf091ab7e38515
Nadezda <nadenka.merkulowa@yandex.ru>
2023-04-12 22:18:36 +0500

    задача15

---
+++ задача2/задача133/Program.cs
@@ -0,0 +1,40 @@
+﻿
+ Console.Write("Введите номер дня недели: ");
+ string text = Console.ReadLine();
+ int numberDayOfWeek = Convert.ToInt32(text);
+
+ if (numberDayOfWeek == 1)
+ {
+ Console.WriteLine("рабочий день");
+ }
+ else if (numberDayOfWeek == 2)
+ {
+ Console.WriteLine("рабочий день");
+ }
+ else if (numberDayOfWeek == 3)
+ {
+ Console.WriteLine("рабочий день");
+ }
+ else if (numberDayOfWeek == 4)
+ {
+ Console.WriteLine("рабочий день");
+ }
+ else if (numberDayOfWeek == 5)
+ {
+ Console.WriteLine("рабочий день");
+ }
+ else if (numberDayOfWeek == 6)
+ {
+ Console.WriteLine("выходной день");
+ }
+ else if (numberDayOfWeek == 7)
+ {
+ Console.WriteLine("выходной день");
+ }
+ else
+ {
+ Console.WriteLine("Нет такого дня");
+ }
+
+
+
