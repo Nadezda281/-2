2da2fb2984e5215db0ce7e17a4e061773bcd7da1
Nadezda <nadenka.merkulowa@yandex.ru>
2023-04-12 17:35:44 +0500

    Задача 10

---
+++ задача10/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random().Next(100, 999); 
+ Console.WriteLine("Случайное число: " + number);
+int first = number / 100; 
+int second= number /10%10;
+
+
+
+Console.Write(second); 
+
+
+
