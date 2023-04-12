cad6aeabeed93d2745b02f3c5e4a120c140d92bb
Nadezda <nadenka.merkulowa@yandex.ru>
2023-04-13 00:41:59 +0500

    задача13

---
+++ задача2/задача133/задача13/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число: ");
+string text = Console.ReadLine();
+int number = Convert.ToInt32(text);
+int GetRank(int n)
+{
+    while (n > 999)
+    {
+        n = n / 10;
+
+    }
+    return n % 10;
+}
+if (number >= 1000)
+{
+    int m = GetRank(number);
+    Console.Write(m);
+}
+if (number > 100 && number < 1000)
+
+{
+    int c = number % 10;
+    Console.WriteLine($"{c}");
+}
+
+if (number < 100)
+{
+    Console.WriteLine("третей цифры нет");
+
+}
+
+
+
+
+
+
